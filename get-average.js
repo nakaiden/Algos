421615ff6715ad088a3259e68844c0b0712d9445
nakaiden <nakaiden@gmail.com>
2021-11-30 02:10:32 -0500

    homeworkpush2

---
+++ get-average.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/563e320cee5dddcf77000158/train/javascript
+// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// Return the average of the given array rounded down to its nearest integer.
+// The array will never be empty.
+
+function getAverage(marks){
+    let total = 0;
+    for(let i = 0; i < marks.length; i++){
+      total += marks[i];
+    }
+    return Math.floor (total / marks.length)
+  }
---
+++ make-upper-case.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/javascript
+//Write a function which converts the input string to uppercase.
+
+//below I used the protoype ".toUpperCase()" to convert all objects within to upper case letters.
+function makeUpperCase(str) {
+    return str.toUpperCase()
+}
